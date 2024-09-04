69f8cf3fe6b1fc1b40915141d83d0bcc7bdc1782
Kam Kennedy <kameronk013@gmail.com>
2024-09-04 13:00:14 -0600

    clarify max possible

--- longest_unique_substring.rb
+++ longest_unique_substring.rb
@@ -2,6 +2,7 @@ def longest_unique_substring(string)
   current_max = 0
   current_count = 0
 
-  max_possible = string.split("").uniq.length
-  require 'pry'; binding.pry
+  max_possible = string.length
+  count_unique_char = string.split("").uniq.length
+
 end
