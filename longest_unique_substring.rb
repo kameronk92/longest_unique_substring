f716e2c6d7d7097a1fa775de85558cf5003ff801
Kam Kennedy <kameronk013@gmail.com>
2024-09-05 08:41:50 -0600

    start if statement

--- longest_unique_substring.rb
+++ longest_unique_substring.rb
@@ -4,5 +4,9 @@ def longest_unique_substring(string)
 
   max_possible = string.length
   count_unique_char = string.split("").uniq.length
+  unique_char_string = string.split("").uniq.join
 
+  if string.include?(unique_char_string)
+    count_unique_char
+  end
 end
