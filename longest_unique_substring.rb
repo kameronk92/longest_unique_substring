d62a4dfbcff71c30d45b643dcc9e0654c9bd0744
Kam Kennedy <kameronk013@gmail.com>
2024-09-05 08:47:55 -0600

    case for shorter substring

--- longest_unique_substring.rb
+++ longest_unique_substring.rb
@@ -1,4 +1,6 @@
 def longest_unique_substring(string)
+  string = string.downcase
+  
   current_max = 0
   current_count = 0
 
@@ -8,5 +10,8 @@ def longest_unique_substring(string)
 
   if string.include?(unique_char_string)
     count_unique_char
+  elsif string.include?(unique_char_string.chop)
+    count_unique_char - 1
+  else
   end
 end
--- longest_unique_substring_spec.rb
+++ longest_unique_substring_spec.rb
@@ -2,6 +2,10 @@ require './longest_unique_substring.rb'
 
 RSpec.describe 'longest_unique_substring' do
   it 'counts the number of characters in the longest substring with all unique characters' do
-    expect(longest_unique_substring('abcabcbb')).to eq(3)
+    expect(longest_unique_substring('aBcabcbb')).to eq(3)
+  end
+
+  it 'case where longest substring is shorter than num unique characters' do
+    expect(longest_unique_substring('abAccbb')). to eq(2)
   end
 end
